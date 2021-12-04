423ecd07352cd454e3a1a53df9b881ebe4860498
Crowdbotics <team@crowdbotics.com>
2021-12-04 15:10:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "mutongoi_fm_32272",
+  url: "https://mutongoi_fm_32272.botics.co",
+  api: "https://mutongoi_fm_32272.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
